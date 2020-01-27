36ab486ebd1776ea7b8f87dc6dce0bc639df5497
wenjie <jinwenjie@live.com>
2020-01-28 01:55:15 +0800

    chore(package): add i18n

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   'transpileDependencies': [
     'vuetify'
-  ]
+  ],
+
+  pluginOptions: {
+    i18n: {
+      locale: 'cn',
+      fallbackLocale: 'en',
+      localeDir: 'locales',
+      enableInSFC: false
+    }
+  }
 }
