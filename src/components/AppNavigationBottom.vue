50c058afdd21848851a643d1c436ce0745669f33
wenjie <jinwenjie@live.com>
2020-03-21 11:51:18 +0800

    fix(app): fix class conflict in navigationbottom

--- src/components/AppNavigationBottom.vue
+++ src/components/AppNavigationBottom.vue
@@ -4,17 +4,26 @@
     app
     grow
   >
-    <v-btn value="recent">
+    <v-btn
+      value="recent"
+      height="inherit"
+    >
       <span>Recent</span>
       <v-icon>mdi-history</v-icon>
     </v-btn>
 
-    <v-btn value="favorites">
+    <v-btn
+      value="favorites"
+      height="inherit"
+    >
       <span>Favorites</span>
       <v-icon>mdi-heart</v-icon>
     </v-btn>
 
-    <v-btn value="nearby">
+    <v-btn
+      value="nearby"
+      height="inherit"
+    >
       <span>Nearby</span>
       <v-icon>mdi-map-marker</v-icon>
     </v-btn>
