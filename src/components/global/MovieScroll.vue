5c722b1940c2d26fd5350c2bc6fa2139dc14225b
wenjie <jinwenjie@live.com>
2020-03-31 23:34:08 +0800

    feat(components/moviescroll): add skeleton loader

--- src/components/global/MovieScroll.vue
+++ src/components/global/MovieScroll.vue
@@ -2,43 +2,55 @@
   <v-sheet
     class="mx-auto"
   >
-    <p class="title ml-2 mb-0">{{genre}}</p>
-    <v-slide-group
-      center-active
-      v-model="model"
-      class="mt-n4 mx-1"
+    <v-skeleton-loader
+      :loading="loading"
+      type="chip"
+      transition="fade-transition"
     >
-      <v-slide-item
-        v-for="n in 10"
-        :key="n"
-        v-slot:default="{ active, toggle }"
-        class="mx-1"
+      <p class="title ml-2 mb-0">{{genre}}</p>
+    </v-skeleton-loader>
+    <v-skeleton-loader
+      :loading="loading"
+      type="image"
+      transition="fade-transition"
+    >
+      <v-slide-group
+        center-active
+        v-model="model"
+        class="mt-n4 mx-1"
       >
-        <v-img
-          :gradient="active ? 'to bottom,rgba(64, 64, 64, 0) 0%,rgba(64, 64, 64, 50) 100%' : undefined"
-          :src="detail.poster"
-          class="ma-4"
-          height="180"
-          width="130"
-          @click="toggle"
+        <v-slide-item
+          v-for="n in 10"
+          :key="n"
+          v-slot:default="{ active, toggle }"
+          class="mx-1"
         >
-          <v-row
-            class="fill-height"
-            align="center"
-            justify="center"
+          <v-img
+            :gradient="active ? 'to bottom,rgba(64, 64, 64, 0) 0%,rgba(64, 64, 64, 50) 100%' : undefined"
+            :src="detail.poster"
+            class="ma-4"
+            height="180"
+            width="130"
+            @click="toggle"
           >
-            <v-scale-transition>
-              <v-icon
-                v-if="active"
-                color="white"
-                size="48"
-                v-text="'mdi-close-circle-outline'"
-              ></v-icon>
-            </v-scale-transition>
-          </v-row>
-        </v-img>
-      </v-slide-item>
-    </v-slide-group>
+            <v-row
+              class="fill-height"
+              align="center"
+              justify="center"
+            >
+              <v-scale-transition>
+                <v-icon
+                  v-if="active"
+                  color="white"
+                  size="48"
+                  v-text="'mdi-close-circle-outline'"
+                ></v-icon>
+              </v-scale-transition>
+            </v-row>
+          </v-img>
+        </v-slide-item>
+      </v-slide-group>
+    </v-skeleton-loader>
     <movie-scroll-detail
       :model="model"
       :detail="detail"
@@ -66,6 +78,7 @@ export default {
   data: function () {
     return {
       model: undefined,
+      loading: true,
       detail: undefinedMovie()
     }
   },
@@ -79,6 +92,7 @@ export default {
       console.log(err)
       message({ text: err, type: 'error' })
     })
+    this.loading = false
   }
 }
 </script>
