102e55b3036885eab47876a8bb10aaf927146e28
wenjie <jinwenjie@live.com>
2020-06-05 10:35:10 +0800

    fix(i18n): fix auto lang dosent work in mobile, change fallback to en

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -4,8 +4,24 @@ import messages from '../locales'
 
 Vue.use(VueI18n)
 
+let locale = 'en-US'
+if (localStorage.getItem('vuex')) {
+  locale = JSON.parse(localStorage.getItem('vuex')).locale
+} else {
+  const lang = [
+    ['zh-CN', 'zh'],
+    ['en-US', 'en']
+  ]
+  for (const langArray of lang) {
+    if (langArray.includes(navigator.language)) {
+      locale = langArray[0]
+      break
+    }
+  }
+}
+
 export default new VueI18n({
-  locale: localStorage.getItem('vuex') ? JSON.parse(localStorage.getItem('vuex')).locale : navigator.language,
-  fallbackLocale: 'zh-CN',
+  locale: locale,
+  fallbackLocale: 'en-US',
   messages: messages
 })
